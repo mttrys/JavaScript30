a9c551ae460c279408c42732b23e59a0c84b3c09
matt <mttrys@gmail.com>
2017-10-30 21:15:29 -0700

    feat: implement 03 with JS and CSS varibales. learned: css variables. todo: reformat to jQuery.

--- 03 - CSS Variables/03javascript.js
+++ 03 - CSS Variables/03javascript.js
@@ -1,1 +1,9 @@
-console.log('hello')
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate() {
+  const suffix = this.dataset.sizing || '';
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
